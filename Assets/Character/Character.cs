69c46d6f44a1c46b655b31cdf769fc8e644a0bd0
noutetu <beyoburo@gmail.com>
2024-09-17 14:47:09 +0900

    Charactercreate

--- Assets/Character/Character.cs
+++ Assets/Character/Character.cs
@@ -5,5 +5,32 @@ using UnityEngine;
 [RequireComponent(typeof(CharacterAnimator))]
 public class Character : MonoBehaviour
 {
-    [SerializeField] CharacterBase characterBase;
+    [SerializeField] CharacterBase characterBase; 
+
+    private new string name;
+    private float maxHp;
+    private float atk;
+    private float speed;
+    private float range;
+    private CharacterType characterType;
+
+    private void Awake()
+    {
+        // CharacterBase のデータを Character クラスのフィールドにコピー
+        if (characterBase != null)
+        {
+            name = characterBase.Name;
+            maxHp = characterBase.MaxHp;
+            atk = characterBase.Atk;
+            speed = characterBase.Speed;
+            range = characterBase.Range;
+            characterType = characterBase.CharacterType;
+        }
+        else
+        {
+            Debug.LogError("CharacterBase is not assigned.");
+        }
+    }
+
+    // 他のメソッドで CharacterBase のデータを使用することができます
 }
--- Assets/Character/CharacterBase.cs
+++ Assets/Character/CharacterBase.cs
@@ -10,18 +10,39 @@ public class CharacterBase : ScriptableObject
     //名前
     [SerializeField] new string name;
     public string Name { get => name;}
+
     //体力
     [SerializeField] float maxHp;
     public float MaxHp { get => maxHp;}
+
     //攻撃力
     [SerializeField] float atk;
     public float Atk { get => atk;}
+
     //スピード
     [SerializeField] float speed;
     public float Speed { get => speed;}
+
+    //射程
+    [SerializeField] float range;
+    public float Range { get => range;}
+
     //キャラクタータイプ
     [SerializeField] CharacterType characterType;
     public CharacterType CharacterType { get => characterType;}
+
+    //キャラクター画像
+    [SerializeField] Sprite sprite;
+    public Sprite Sprite { get => sprite;}
+
+    //ソードマンなら射程を0に
+    private void OnEnable() 
+    {
+        if(characterType == CharacterType.SwordMan)
+        {
+            range = 0;
+        }
+    }
 }
 
 public enum CharacterType
